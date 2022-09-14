a1c94dde4f42935ce1c779d1a3c6e814042c6263
Guigak <97238914+Guigak@users.noreply.github.com>
2022-09-14 17:17:36 +0900

    add drill #4
    2019182022 신형섭

---
+++ folder_04/2019182022turtle.py
@@ -0,0 +1,40 @@
+import turtle
+
+count = 5
+
+while count > 0:
+    turtle.forward(500)
+    
+    if count % 2 == 0:
+        turtle.left(90)
+        turtle.forward(100)
+        turtle.left(90)
+    else:
+        turtle.right(90)
+        turtle.forward(100)
+        turtle.right(90)
+
+    count -= 1
+
+turtle.forward(500)
+turtle.right(90)
+
+count = 5
+
+while count > 0:
+    turtle.forward(500)
+
+    if count % 2 == 0:
+        turtle.left(90)
+        turtle.forward(100)
+        turtle.left(90)
+    else:
+        turtle.right(90)
+        turtle.forward(100)
+        turtle.right(90)
+
+    count -= 1
+
+turtle.forward(500)
+        
+turtle.exitonclick()
